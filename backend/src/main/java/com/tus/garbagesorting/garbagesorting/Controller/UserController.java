a57bb5b772551cb129f9fae8dad94b54cd1d7c30
raymond <raymondomon@yahoo.com>
2022-02-14 23:42:03 +0000

    login user

--- backend/src/main/java/com/tus/garbagesorting/garbagesorting/Controller/UserController.java
+++ backend/src/main/java/com/tus/garbagesorting/garbagesorting/Controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class UserController {
@@ -20,26 +21,41 @@ public class UserController {
     private UserMapper userMapper;
 
     @RequestMapping("/AllUsers")
-    public Object findAll(){
-      List<User> list=  userMapper.findAll();
+    public Object findAll() {
+        List<User> list = userMapper.findAll();
         return list;
     }
 
     @RequestMapping("/InsertUser")
-    public Object insert(@RequestBody User user){
+    public Object insert(@RequestBody User user) {
 
-
-        int res=  userMapper.insert(user);
+        int res = userMapper.insert(user);
         return "Account successfully created";
     }
 
     @RequestMapping("/Login")
-    public Object login(@RequestParam String username){
+    public Object login(@RequestParam String email, String password) {
 
-        List<User> list=  userMapper.findAll();
+        List<User> list = userMapper.findAll();
 
-//        list res=  userMapper.findAll();
-        return username;
+        // Validate user email and password.
+        // Check if email and password stored in database matches email and password provided.
+
+        String userEmail;
+        String userPassword;
+
+        for (int i = 0; i < list.size(); i++) {
+            userEmail = list.get(i).getUser_email();
+            userPassword = list.get(i).getUser_password();
+
+            if (email.equalsIgnoreCase(userEmail) && password.equalsIgnoreCase(userPassword)) {
+                return "Successful login";
+
+            } else {
+                return "Incorrect username or password";
+            }
+        }
+        return -1;
     }
 
     // Create Api to check if email is already in use
