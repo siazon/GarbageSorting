682213531909101886078855cf9bead9bce4c380
Jiatingli99 <A00291038@student.ait.ie>
2022-03-01 11:41:55 +0000

    change jquery

--- frontend/loginpage/js/jquery.pure.tooltips.js
+++ frontend/loginpage/js/jquery.pure.tooltips.js
@@ -1,25 +1,20 @@
-/**
- * jQuery带箭头提示框插件
- * email: tianshaojie@msn.com
- * date: 2013-01-15
- * version: 1.0.0
- */
+
 (function($) {
 	var max = Math.max,
 		min = Math.min;
 	$.pt = $.pureToolTips = function(options) {
 		var opts = $.extend({
-			target 		: null,		//目标元素，不能为空
-			position 	: 't',		//提示框相对目标元素位置 t=top,b=bottom,r=right,l=left
-			align		: 'c',		//提示框与目标元素的对齐方式，自动调节箭头显示位置，指向目标元素中间位置，c=center, t=top, b=bottom, l=left, r=right [postion=t|b时，align=l|r有效][position=t|b时，align=t|d有效]
-			arrow		: true,		//是否显示箭头
-			content 	: '',		//内容
-			width 		: 200,		//宽度
-			height 		: 'auto',	//高度
-			autoClose 	: true,		//是否自动关闭
-			time 		: 2000,		//自动关闭延时时长
-			leaveClose 	: false,	//提示框失去焦点后关闭
-			close 		: null		//关闭回调函数
+			target 		: null,		
+			position 	: 't',		
+			align		: 'c',		
+			arrow		: true,		
+			content 	: '',		
+			width 		: 200,		
+			height 		: 'auto',	
+			autoClose 	: true,		
+			time 		: 2000,		
+			leaveClose 	: false,	
+			close 		: null		
 		}, options || {}),
 
 		$ao, $ai, w, h,
@@ -34,10 +29,10 @@
 		align = opts.align,
 		arrow = opts.arrow,
 
-		constant = {b:'pt-up', t:'pt-down', r:'pt-left', l:'pt-right'}, //相对位置正好和箭头方向相反
+		constant = {b:'pt-up', t:'pt-down', r:'pt-left', l:'pt-right'}, 
 		arrowClass = constant[position] || constant.t;
 
-		//初始化元素，事件
+		
 		function init() {
 			if(!opts.target) {
 				return;
@@ -50,13 +45,13 @@
 			$ai =  $pt.find('.in').toggle(arrow);
 			w = $pt.outerWidth();
 			h = $pt.outerHeight();
-			arrow && autoAdjust();			//设置箭头自动居中
-			$pt.css(setPos()).show();		//设置显示框位置和自动隐藏事件
-			opts.leaveClose && leaveClose();//离开关闭
-			opts.autoClose && !opts.leaveClose && autoClose(opts.timeout);	//默认自动关闭，优先离开关闭
+			arrow && autoAdjust();			
+			$pt.css(setPos()).show();		
+			opts.leaveClose && leaveClose();
+			opts.autoClose && !opts.leaveClose && autoClose(opts.timeout);	
 			return $pt;
 		}
-		//计算提示框应该出现在目标元素位置
+		
 		function setPos() {
 			var btw = arrow ? parseInt($ao.css('border-top-width'), 10) : 3,
 				brw = arrow ? parseInt($ao.css('border-right-width'), 10) : 3,
@@ -79,7 +74,7 @@
 			return result;
 		}
 
-		//设置箭头自动居中
+		
 		function autoAdjust() {
 			var aop, aip, bw, auto='auto';
 			switch(position) {
@@ -108,7 +103,7 @@
 					aip = {top:aop.top+1, left:aop.left+1, right:auto, bottom:auto};
 					break;
 			}
-			//上下侧，左右对齐
+			
 			function alignLR() {
 				if(align === 'l' && width/2 > bw && width/2 < w-bw) {
 					aop.left = width/2-bw/2;
@@ -116,7 +111,7 @@
 					aop.left = w-width/2-bw/2;
 				}
 			}
-			//左右侧，上下对齐
+			
 			function alignTB() {
 				if(align === 't' && height/2 > bw && height/2 < h-bw) {
 					aop.top = height/2 - bw;
@@ -127,7 +122,7 @@
 			$ao.css(aop);
 			$ai.css(aip);
 		}
-		//设置提示框自动关闭
+		
 		function autoClose() {
 			window.ptt && clearTimeout(ptt);
 			window.pta && clearTimeout(pta);
@@ -136,9 +131,9 @@
 				$.isFunction(opts.close) && opts.close();
 			}, opts.time);
 		}
-		//设置提示框失去焦点关闭
+		
 		function leaveClose() {
-			//先解绑再绑定，不然会形成事件链
+			
 			$pt.unbind('mouseleave').mouseleave(function(e) {
 				$pt.hide();
 				$.isFunction(opts.close) && opts.close();
@@ -149,7 +144,7 @@
 		return init();
 	};
 
-	//扩展到包装集上
+	
 	$.fn.pt = $.fn.pureToolTips = function(options) {
 		var opts = $.extend({
 			leaveClose:true
