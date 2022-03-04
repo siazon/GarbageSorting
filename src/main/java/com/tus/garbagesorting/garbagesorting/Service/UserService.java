9242851f78469a78f47b5cd91f4f8a5eefc11db9
Xiasong Chen <296724771@qq.com>
2022-03-04 19:05:26 +0000

    merge test code

--- src/main/java/com/tus/garbagesorting/garbagesorting/Service/UserService.java
+++ src/main/java/com/tus/garbagesorting/garbagesorting/Service/UserService.java
@@ -5,21 +5,27 @@ import com.tus.garbagesorting.garbagesorting.Model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Repository
-public class UserService implements UserMapper
-{
+public class UserService implements UserMapper {
+
+    public User user;
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Override
-    public int insert(User user) {
-        return jdbcTemplate.update(
-                "insert into tb_user (user_name, user_email, user_phone, user_password, user_role, user_status) values(?,?,?,?,?,?)",
-                user.getUser_name(), user.getUser_email(), user.getUser_phone(), user.getUser_password(), user.getUser_role(), user.getUser_status());
+    public User insert(User user) {
+        int a = jdbcTemplate.update(
+                "insert into tb_user (user_name, user_email, user_phone, user_password, user_role, user_status) " +
+                        "values(?,?,?,?,?,?)",
+                user.getUser_name(), user.getUser_email(), user.getUser_phone(), user.getUser_password(),
+                user.getUser_role(), user.getUser_status());
+
+        return user;
+
     }
 
     @Override
@@ -30,11 +36,16 @@ public class UserService implements UserMapper
     }
 
     @Override
-    public int deleteById(int user_email) {
-        return jdbcTemplate.update(
-                "delete tb_user where user_email = ?",
-                user_email);
+    public int deleteById(int id) {
+        return jdbcTemplate.update("DELETE FROM tb_user WHERE id=?", id);
     }
+
+//    @Override
+//    public int findById(int id) {
+//        var rowMapper = BeanPropertyRowMapper.newInstance(User.class);
+//
+//    }
+
 
     @Override
     public List<User> findAll() {
@@ -42,6 +53,7 @@ public class UserService implements UserMapper
                 "select * from tb_user",
                 (rs, rowNum) ->
                         new User(
+                                rs.getInt("id"),
                                 rs.getString("user_email"),
                                 rs.getString("user_name"),
                                 rs.getString("user_phone"),
