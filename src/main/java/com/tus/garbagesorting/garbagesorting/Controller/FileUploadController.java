9a942b162e9f17cc37c3a50a9cce6205d6ceddd2
Xiasong Chen <296724771@qq.com>
2022-03-14 11:09:02 +0000

    add upload backend

--- src/main/java/com/tus/garbagesorting/garbagesorting/Controller/FileUploadController.java
+++ src/main/java/com/tus/garbagesorting/garbagesorting/Controller/FileUploadController.java
@@ -1,52 +1,47 @@
 package com.tus.garbagesorting.garbagesorting.Controller;
 
+import com.tus.garbagesorting.garbagesorting.Service.ImageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletContext;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
+@CrossOrigin
 @Controller
 public class FileUploadController {
 
+    @Autowired
+    ServletContext context;
     //Save the uploaded file to this folder
-    private static String UPLOADED_FOLDER = "F://temp//";
+    private static String UPLOADED_FOLDER = "D://temp//";
 
-    @GetMapping("/")
+    @GetMapping("/uploader")
     public String index() {
         return "upload";
     }
 
-    @PostMapping("/upload") // //new annotation since 4.3
-    public String singleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
-
-        if (file.isEmpty()) {
-            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
-            return "redirect:uploadStatus";
-        }
-
+    @PostMapping("/upload/{type}") // //new annotation since 4.3
+    public ResponseEntity<?> singleFileUpload(@RequestParam("file") MultipartFile file, @PathVariable String type) {
+        String iType = type;
+        String path = new FileSystemResource("").getFile().getAbsolutePath() + "\\frontend\\img\\temp";
+        String fileName = file.getOriginalFilename();
         try {
-
-            // Get the file and save it somewhere
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            Files.write(path, bytes);
-
-            redirectAttributes.addFlashAttribute("message",
-                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            file.transferTo(new File(path + "\\" + fileName));
+            ChangeImgSize(path, fileName);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-
-        return "redirect:/uploadStatus";
+        return ResponseEntity.ok("File uploaded successfully.");
     }
 
     @GetMapping("/uploadStatus")
@@ -54,4 +49,19 @@ public class FileUploadController {
         return "uploadStatus";
     }
 
+    public void ChangeImgSize(String path, String fileName) {
+        try {
+            //读取原始图片
+            BufferedImage image = ImageIO.read(new FileInputStream(path + "\\" + fileName));
+            System.out.println("Width: " + image.getWidth());
+            System.out.println("Height: " + image.getHeight());
+            //Resize
+            BufferedImage newImage = ImageUtils.resizeImage(image, 100, 100);
+            //
+            String _path = new FileSystemResource("").getFile().getAbsolutePath() + "\\frontend\\img\\temp";
+            ImageIO.write(newImage, "png", new File(new FileSystemResource("").getFile().getAbsolutePath() + "\\frontend\\img\\" + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/com/tus/garbagesorting/garbagesorting/Service/ImageUtils.java
@@ -0,0 +1,47 @@
+package com.tus.garbagesorting.garbagesorting.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class ImageUtils {
+    /**
+     * 通过BufferedImage图片流调整图片大小
+     */
+    public static BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
+        Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_AREA_AVERAGING);
+        BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+        outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+        return outputImage;
+    }
+
+    /**
+     * BufferedImage图片流转byte[]数组
+     */
+    public static byte[] imageToBytes(BufferedImage bImage) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(bImage, "jpg", out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toByteArray();
+    }
+
+    /**
+     * byte[]数组转BufferedImage图片流
+     */
+    private static BufferedImage bytesToBufferedImage(byte[] ImageByte) {
+        ByteArrayInputStream in = new ByteArrayInputStream(ImageByte);
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+}
