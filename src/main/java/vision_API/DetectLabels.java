f9516ab320268cdf935b06e41a820ef429ef6781
Eshaq Rahmani <eshaq-rahmani@hotmail.com>
2022-03-16 22:13:01 +0000

    info page added slider

--- src/main/java/vision_API/DetectLabels.java
+++ src/main/java/vision_API/DetectLabels.java
@@ -8,13 +8,11 @@ import com.google.cloud.vision.v1.Feature;
 import com.google.cloud.vision.v1.Image;
 import com.google.cloud.vision.v1.ImageAnnotatorClient;
 import com.google.protobuf.ByteString;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-
-
 
 
 import org.springframework.boot.SpringApplication;
@@ -23,12 +21,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
 public class DetectLabels {
 
     public static void detectLabels() throws IOException {
         // TODO(developer): Replace these variables before running the sample.
-        String filePath = "src/main/java/vision_API/botttlee.jpg";
+        String filePath = "src/main/java/vision_API/sains.jpg";
         detectLabels(filePath);
     }
 
@@ -58,12 +55,28 @@ public class DetectLabels {
                 }
 
                 // For full list of available annotations, see http://g.co/cloud/vision/docs
+                int limit = 0;
                 for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
-                    annotation
-                            .getAllFields()
-                            .forEach((k, v) -> System.out.format("%s : %s%n", k, v.toString()));
+
+                    float score = annotation.getScore();
+                    String description = annotation.getDescription();
+                
+
+                    if (score > 0.5) {
+                        if (description.toLowerCase().contains("glass")) {
+                            System.out.println("");
+                            System.out.println("This object is recyclable");
+                        } else if (description.toLowerCase().contains("plastic")) {
+                            System.out.println("");
+                            System.out.println("This object is garbage");
+                        }
+
+                    }
                 }
+
+
             }
         }
     }
+
 }
